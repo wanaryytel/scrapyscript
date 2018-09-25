d6cf71c74988a53138d55b22cd2790b4eccf980a
Hanno <hannosarg@outlook.com>
2018-09-25 09:54:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Scrapy==1.4.0',
+    install_requires=['Scrapy>=1.5.0',
                       'billiard==3.5.0.2',
     ],
 
